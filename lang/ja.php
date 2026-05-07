6fbe923975a8c11d5950496817b7ee6a51975804
Joe Huss <detain@interserver.net>
2026-05-07 05:50:43 -0400

    Add Japanese locale for all libs (#254)

---
+++ lang/ja.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Japanese translations for candy-shine.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'theme.read_failed'   => 'テーマファイルを読み取れません：{path}',
+    'theme.json_object'   => 'テーマ JSON はオブジェクトにデコードされる必要があります',
+    'renderer.unknown_style' => '不明な標準スタイル：{name}',
+];
