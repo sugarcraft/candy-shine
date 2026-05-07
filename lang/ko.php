c8fe19ce06edb5165b42a44d7c7ba4fc18f231f5
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Korean translations for candy-shine.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'theme.read_failed'   => '테마 파일을 읽을 수 없음: {path}',
+    'theme.json_object'   => '테마 JSON은 객체로 디코딩되어야 합니다',
+    'renderer.unknown_style' => '알 수 없는 표준 스타일: {name}',
+];
