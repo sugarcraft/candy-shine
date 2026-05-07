f7a7f7840896d5ca7ca0c68cc2db9427de757f00
Joe Huss <detain@interserver.net>
2026-05-07 05:45:19 -0400

    Add remaining Portuguese locale files for all libs (#251)

---
+++ lang/pt.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Portuguese translations for candy-shine.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'theme.read_failed'   => 'não foi possível ler o ficheiro de tema: {path}',
+    'theme.json_object'   => 'o JSON do tema tem de descodificar para um objeto',
+    'renderer.unknown_style' => 'estilo padrão desconhecido: {name}',
+];
